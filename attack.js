882cbd562f522ccaa00529457b6343415ab857fc
Diapostrofo <Diapostrofo@gmail.com>
2022-08-17 15:22:00 +0200

    updatet action with tests

--- attack.js
+++ attack.js
@@ -1,3 +1,3 @@
-const attack = (creatureName, damage, isCritical) => `${creatureName} dealt ${isCritical ? damage * 2 : damage} damage!`;
+const attack = (creatureName, damage, isCritical) => `${creatureName} dealt a wopping ${isCritical ? damage * 2 : damage} damage!`;
 
 module.exports = attack;
