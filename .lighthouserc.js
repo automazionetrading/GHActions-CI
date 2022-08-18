4749ef0797143fd6e6ae0fc5267e8012e65950dc
Diapostrofo <Diapostrofo@gmail.com>
2022-08-18 16:30:11 +0200

    ulteriori check CI

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -6,8 +6,12 @@ module.exports = {
     assert: {
       preset: 'lighthouse:no-pwa',
       assertions: {
-        'csp-xss': 'off',
-        'errors-in-console': 'off'
+        'csp-xss': 'warn',
+        'errors-in-console': 'warn',
+        'categories:performance': ['error', { minScore: 0.95 }],
+        'categories:accessibility': ['error', { minScore: 0.95 }],
+        'categories:best-practices': ['error', { minScore: 0.95 }],
+        'categories:seo': ['error', { minScore: 1 }]
       }
     }
   },
