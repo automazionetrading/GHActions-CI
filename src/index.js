388a6096badf70d829e4d1d1176e79979b435259
Diapostrofo <Diapostrofo@gmail.com>
2022-08-18 14:06:20 +0200

    add html website functionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import attack from './attack';
 
-console.log(
-  attack('Walrus', 24),
-  attack('Goblin', 42),
-);
+const resultTag = document.querySelector('#resultTag');
+const creature = document.querySelector('#creature');
+const damage = document.querySelector('#damage');
+const resultButton = document.querySelector('#resultButton');
+
+resultButton.addEventListener('click', () => {
+  const result = attack(creature.value, damage.value);
+
+  resultTag.innerHTML = result;
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: 'bael-loader',
+        use: 'babel-loader',
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
 };
